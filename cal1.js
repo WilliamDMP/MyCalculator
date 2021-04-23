07dedb7c0e21b374ab36cd1964b5f7318781cd17
William David Martinez Perez <wdmartinez89@gmail.com>
2021-04-23 12:23:10 -0500

    Added function to evaluate display actions

--- cal1.js
+++ cal1.js
@@ -1,3 +1,12 @@
 function valor(x) {
     document.getElementById('display').value += x
 }
+
+function borrardisplay(y) {
+    document.getElementById('display').value = y
+}
+
+function cal_resultado() {
+    let result = eval(document.getElementById('display').value) 
+    document.getElementById('display').value = result
+}
